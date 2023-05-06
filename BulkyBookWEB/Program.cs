7303190e4d3dd892140dbe1c063151df2561dd82
Rufat Valiyev <rufat.valiyev02@gmail.com>
2023-05-06 14:13:02 +0200

    Test

--- BulkyBookWEB/Program.cs
+++ BulkyBookWEB/Program.cs
@@ -47,7 +47,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())  //request goes back and forth through pipeline!
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 dayss. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 daysd. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
